c1e92d011d073850c35f9c6bb53531ae3726f6f7
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-23 18:14:26 -0300

    Segunda Atualização

--- app/src/main/java/com/lucasesteves/rogalabsapi/Activity/MainActivity.kt
+++ app/src/main/java/com/lucasesteves/rogalabsapi/Activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lucasesteves.rogalabsapi
+package com.lucasesteves.rogalabsapi.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucasesteves.rogalabsapi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
