15115c5dc57698305d06d78c38ceed10cb52efd6
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-07-24 13:51:38 -0300

    PrimeiroUpdate

--- app/src/main/java/com/lucasesteves/rogalabsapi/Activity/MainActivity.kt
+++ app/src/main/java/com/lucasesteves/rogalabsapi/Activity/MainActivity.kt
@@ -28,28 +28,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        fun showRecycleView(Posts: List<Posts>) {
-            val postAdapter = PostsAdapter(Posts) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "${it.title} - ${it.body}",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            with(binding) {
-                postsRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-                postsRecyclerView.adapter = postAdapter
 
-            }
-        }
         callback.enqueue(object : Callback<List<Posts>> {
             override fun onFailure(call: Call<List<Posts>>, t: Throwable) {
                 Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
             }
 
             override fun onResponse(call: Call<List<Posts>>, response: Response<List<Posts>>) {
-                response.body()?.forEach { body ->
-                    showRecycleView(listOf(Posts(body.userId, body.id, body.title, body.body)))
+                response.body()?.forEach { it ->
+                    val postList = listOf(Posts(it.userId, it.id,it.title, it.body))
+                    val postAdapter = PostsAdapter(postList) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "${it.title} - ${it.body}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    with(binding) {
+                        postsRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                        postsRecyclerView.adapter = postAdapter
+
+                    }
                 }
             }
         })
